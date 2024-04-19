be932858ff5f1949e50c9dadd87acd21313d1dd2
Nmmmm222 <alisawu0606@gmail.com>
2024-04-19 12:04:37 +0800

    week09

---
+++ week09/week09-2.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int myFuncA()
+{
+    int x=200;
+    printf("myFUncA()裡的X是%d\n",x);
+
+}
+int main()
+{
+    int x=100;
+    printf("main()裡的x是:%d\n",x);
+    myFuncA();
+    printf("main()裡的x是:%d\n",x);
+}
+
